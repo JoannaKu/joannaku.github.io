a256b5c683d0216f15c49e6abd81f7bf215532ff
Asia <j.kusmider@o2.pl>
2018-02-01 15:38:48 +0100

    add rwd

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var myVar;
+
+function loaderFunction() {
+    myVar = setTimeout(showPage, 4000);
+    document.getElementById("myDiv").style.display = "none";
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("loader-body").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
